d9d9b3636af19cf72c33adcb9e022b62bb840d13
Sandra Arana Gisbert <sandra.94g@gmail.com>
2025-10-07 19:54:26 +0200

    new

--- T1/ProyectoInicial/src/Main.java
+++ T1/ProyectoInicial/src/Main.java
@@ -1,9 +1,28 @@
 public class Main {
 
     static void main(String[] args) {
-        System.out.println("Hola, este es mi primer programa en Java");
+        String nombreCompleto="Sandra Arana";
+        int edad = 31;
+        edad = edad +1;
+        float altura = 1.58f;
+        double peso = 43.55;
+        char letraCarnet = 'B';
+        boolean experiencia = false;
+        final String NUMERO_DNI = "53724241J";
+
+
+
+        System.out.println("Mi nombre completo es "+nombreCompleto);
+        System.out.println("Mi edad es "+edad+" años");
+        System.out.println("Mi altura es "+altura+" metros");
+        System.out.println("Mi peso es "+peso+" kg");
+        System.out.println("La letra de mi carnet de conducir es "+letraCarnet);
+        System.out.println("¿Tengo experiencia? "+experiencia);
 
 
     }
 
+
+
 }
+
