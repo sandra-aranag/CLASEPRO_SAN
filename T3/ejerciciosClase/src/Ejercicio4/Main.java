f544463da9515877c5498ab58e0983eb4e88a167
Sandra Arana Gisbert <sandra.94g@gmail.com>
2025-11-15 11:53:50 +0100

    update

--- T3/ejerciciosClase/src/Ejercicio4/Main.java
+++ T3/ejerciciosClase/src/Ejercicio4/Main.java
@@ -1,5 +1,7 @@
 package Ejercicio4;
 
+import java.util.Scanner;
+
 public class Main {
 
     /*4. Crea un programa para jugar una partida virtual de dardos entre dos
@@ -19,10 +21,7 @@ pasa" y su total no cambia ese turno.
 ganando (el que tiene menos puntos).
 • Al finalizar, muestra el ganador y cuántos turnos tomó la partida.*/
 
-
-    public static void main(String[] args) {
-
-        //Lo primero que debe tener son dos puntuaciones: PuntuacionJ1=301; puntuacionJ2=302;
+    //Lo primero que debe tener son dos puntuaciones: PuntuacionJ1=301; puntuacionJ2=302;
 
         /* DO-WHILE
 
@@ -32,9 +31,114 @@ ganando (el que tiene menos puntos).
 
         Mientras uno de los dos jugadores no haya llegado a 0, continua.
         Esto sería un do-while porque, como mínimo, debo hacer una tirada
+
+        Se crea un FOR por cada jugador.
          */
 
-        //NO HAY QUE HACER SWITCH.
+    //NO HAY QUE HACER SWITCH.
+
+
+    public static void main(String[] args) {
+
+        //pido nombres por consola
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jugador 1, escribe tu nombre: ");
+        String nombreJ1 = scanner.nextLine();
+        System.out.println("Jugador 2, escribe tu nombre: ");
+        String nombreJ2 = scanner.nextLine();
+
+        //declaro variables
+        int totalJ1 = 301, totalJ2=301, lanzamiento = 0, turnos=0;
+
+        //muestro en consola el total inicial de cada jugador y explico la dinámica
+        System.out.println("\n-----------------------");
+        System.out.printf("Puntuación inicial del jugador 1: %d%n", totalJ1);
+        System.out.printf("Puntuación inicial del jugador 2: %d%n", totalJ2);
+        System.out.println("\n-----------------------");
+        System.out.println("¡Que empiece el juego! ¡Gana el que primero llegue a 0!");
+
+
+        do{
+            turnos++;
+
+            //empieza el turno del jugador 1
+            System.out.println("\n-----------------------");
+            System.out.println("¡Tira tres veces el dado, " +nombreJ1+ "!");
+            int puntosRondaJ1=0;
+
+            for (int i = 1; i <= 3; i++) {
+                lanzamiento= (int) (Math.random()*61);
+                puntosRondaJ1 += lanzamiento;
+                System.out.println("Dardo " + i + ": " + lanzamiento);
+            }
+            System.out.println("-----------------------");
+            System.out.printf("Total del turno: %d%n",puntosRondaJ1);
+
+            //¿Se pasa?
+            if (totalJ1 - puntosRondaJ1 < 0) {
+                System.out.println("¡Te has pasado, " +nombreJ1+ "! Por lo tanto, la puntuación no cambia.");
+
+            } else {
+                totalJ1-=puntosRondaJ1;
+            }
+            System.out.println("La puntuación actual de " +nombreJ1+ " es de " +totalJ1);
+
+            //Y si ha ganado
+            if (totalJ1 == 0){
+                break;
+            }
+
+
+
+            //empieza el turno del jugador 2
+            System.out.println("\n-----------------------");
+            System.out.println("¡Tira tres veces el dado, " +nombreJ2+ "!");
+            int puntosRondaJ2=0;
+
+            for (int i = 1; i <= 3; i++) {
+                lanzamiento= (int) (Math.random()*61);
+                puntosRondaJ2 += lanzamiento;
+                System.out.println("Dardo " + i + ": " + lanzamiento);
+
+            }
+            System.out.println("-----------------------");
+            System.out.printf("Total del turno: %d%n",puntosRondaJ2);
+
+            //¿Se pasa?
+            if (totalJ2 - puntosRondaJ2 < 0){
+                System.out.println("¡Te has pasado, " +nombreJ2+ "! Por lo tanto, la puntuación no cambia.");
+            } else {
+                totalJ2-=puntosRondaJ2;
+            }
+            System.out.println("La puntuación actual de " +nombreJ2+ " es de " +totalJ2);
+
+            //Y si ha ganado
+            if (totalJ2 == 0){
+                break;
+            }
+
+            //¿Y quién gana?
+            if(totalJ1 > totalJ2){
+                System.out.println("¡Va ganando " +nombreJ1+ "!");
+            } else if (totalJ2 > totalJ1){
+                System.out.println("¡Va ganando " +nombreJ2+ "!");
+            } else {
+                System.out.println("¡" +nombreJ1+ " y " +nombreJ2+ " van empatados!");
+            }
+
+        }while (totalJ1 > 0 && totalJ2 > 0);
+
+        System.out.println("-----------------------");
+        System.out.println("¡LA PARTIDA HA TERMINADO!");
+        System.out.println("-----------------------");
+
+        if (totalJ1 == 0){
+            System.out.println("¡Ha ganado " +nombreJ1+ "!");
+        } else {
+            System.out.println("¡Ha ganado " +nombreJ2+ "!");
+        }
+
+        System.out.println("El número de total de turnos jugados es de " +turnos);
 
     }
 
