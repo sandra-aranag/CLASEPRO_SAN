4529e1bb7f83511cd2b2091a5cfd13e149ffea6c
Sandra Arana Gisbert <sandra.94g@gmail.com>
2025-12-15 20:12:28 +0100

    commit terminal poo

--- T5/ejerciciosBase/src/Entrada.java
+++ T5/ejerciciosBase/src/Entrada.java
@@ -19,25 +19,34 @@ public class Entrada {
         double peso = scanner.nextDouble();
         System.out.println("Dime la altura: ");
         int altura = scanner.nextInt();
+        System.out.println("Indica tu género (M/F): ");
+        char genero = scanner.next().charAt(0);
 
-        Persona p1 = new Persona(nombre, apellido, dni, edad, altura, peso);
-        Persona p2 = new Persona("Marta", "Hernandez");
-        Persona p3 = new Persona();
+        Persona p1 = new Persona(nombre, apellido, dni, edad, altura, peso, genero);
+       // Persona p2 = new Persona("Marta", "Hernandez");
+       // Persona p3 = new Persona();
 
 
         //cambiar edad a 20
         p1.setEdad(20);
-        p2.setEdad(20);
-        p3.setEdad(20);
+      //  p2.setEdad(20);
+      //  p3.setEdad(20);
+      //  p2.incrementarEdad(5);
+
 
         System.out.println("----- PERSONA 1 -----");
         p1.mostrarDatos();
 
-        System.out.println("----- PERSONA 2 -----");
+    /*    System.out.println("----- PERSONA 2 -----");
         p2.mostrarDatos();
 
         System.out.println("----- PERSONA 3 -----");
-        p3.mostrarDatos();
+        p3.mostrarDatos();*/
+
+        System.out.println("----- CALCULADORA DEL IMC -----");
+        System.out.printf("IMC: %.2f%n", p1.calculadoraImc());
+        System.out.println("Tu estado físico es de: " + p1.estadoFisico());
+
 
         scanner.close();
     }
--- T5/ejerciciosBase/src/model/Persona.java
+++ T5/ejerciciosBase/src/model/Persona.java
@@ -5,10 +5,10 @@ import lombok.*;
 @Data //getter y setters
 
 public class Persona {
-
     private String nombre, apellido, dni;
     private int edad, altura;
     private double peso;
+    private char genero;
 
     public void mostrarDatos() {
         System.out.println("Nombre: " + nombre);
@@ -17,25 +17,29 @@ public class Persona {
         System.out.println("Edad: " + edad);
         System.out.println("Altura: " + altura);
         System.out.println("Peso: " + peso);
+        System.out.println("Género: " + genero);
+
     }
 
-    public Persona(){
+    public Persona() {
         this.nombre = "Datos por defecto";
         this.apellido = "Datos por defecto";
         this.dni = "111111111X";
         this.edad = 0;
         this.altura = 0;
         this.peso = 0;
+        this.genero = 0;
 
     }
 
-    public Persona(String nombre, String apellido, String dni, int edad, int altura, double peso) {
+    public Persona(String nombre, String apellido, String dni, int edad, int altura, double peso, char genero) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
         this.edad = edad;
         this.altura = altura;
         this.peso = peso;
+        this.genero = genero;
     }
 
     public Persona(String nombre, String apellido, String dni, int edad) {
@@ -45,6 +49,7 @@ public class Persona {
         this.edad = edad;
         this.peso = 0;
         this.altura = 0;
+        this.genero = 'U';
     }
 
     public Persona(String nombre, String apellido) {
@@ -54,11 +59,60 @@ public class Persona {
         this.edad = 0;
         this.altura = 0;
         this.peso = 0;
+        this.genero = 0;
 
     }
 
+    public void incrementarEdad(int incremento) {
+        this.edad += incremento;
+    }
+
+    //Un metodo que calcule y devuelva el IMC. Para calcular el imc se aplica la siguiente formula
+    // IMC = peso / (altura * altura)
+
+    public double calculadoraImc() {
+        double alturaMetros = this.altura / 100.0;
+        return this.peso / (alturaMetros * alturaMetros);
+    }
+
+    public String estadoFisico() {
+        double imc = calculadoraImc();
+
+        if (genero == 'M'){
+            return estadoHombre(imc);
+        }else{
+            return estadoMujer(imc);
+        }
 
 
 
+    }
 
+    private String estadoHombre(double imc){
+        if (imc < 20){
+            return "Bajo peso";
+        } else if (imc >= 20 && imc <=27) {
+            return "Normopeso";
+        }else if (imc >=27 && imc <=30){
+            return "Sobrepeso";
+        } else if (imc >=30 && imc <=40) {
+            return "Obesidad";
+        }else {
+            return "Obsesidad mórbida";
+        }
+    }
+
+    private String estadoMujer(double imc){
+        if (imc < 20){
+            return "Bajo peso";
+        } else if (imc >= 20 && imc <=25) {
+            return "Normopeso";
+        }else if (imc >=25 && imc <=30){
+            return "Sobrepeso";
+        } else if (imc >=30 && imc <=40) {
+            return "Obesidad";
+        }else {
+            return "Obsesidad mórbida";
+        }
+    }
 }
